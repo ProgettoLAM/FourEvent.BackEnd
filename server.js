0863eb1ed729e9a9050249d7fec2f58566f8f52d
Gianmarco Spinaci <spino9330@gmail.com>
2016-03-17 14:53:28 +0100

    Creato il nuovo server per Azure

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 /**
  * Created by Gianmarco on 17/03/2016.
  */
+var http = require("http");
 
-var http = require('http')
-var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+http.createServer(function (request, response) {
+
+    // Send the HTTP header
+    // HTTP Status: 200 : OK
+    // Content Type: text/plain
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+
+    // Send the response body as "Hello World"
+    response.end('Hello World\n');
+}).listen(8081);
