4a64316c553bba9248ee2244f108014934e880d7
Gianmarco Spinaci <spino9330@gmail.com>
2016-03-17 12:04:20 +0100

    Aggiunto il server di backend

---
+++ server.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Gianmarco on 17/03/2016.
+ */
+
+var http = require('http');
+var port = process.env.PORT || 1337;
+http.createServer(function(req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World\n');
+}).listen(port);
