424f1f3586affcd1a5ec895c0e1763924d0ac526
Gianmarco Spinaci <spino9330@gmail.com>
2016-03-17 13:53:56 +0100

    Introduzione

--- server.js
+++ server.js
@@ -2,22 +2,4 @@
  * Created by Gianmarco on 17/03/2016.
  */
 
-var express = require('express');
-var app = express();
-var fs = require("fs");
-
-app.get('/listUsers', function (req, res) {
-    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
-        console.log( data );
-        res.end( data );
-    });
-})
-
-var server = app.listen(8081, function () {
-
-    var host = server.address().address
-    var port = server.address().port
-
-    console.log("Example app listening at http://%s:%s", host, port)
-
-})
+console.log("Hello, World!")
