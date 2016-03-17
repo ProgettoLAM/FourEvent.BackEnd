b385164e1f8eb697978728c5f9f5e869a1b9bf07
Gianmarco Spinaci <spino9330@gmail.com>
2016-03-17 13:50:57 +0100

    Aggiunto list users

--- server.js
+++ server.js
@@ -2,9 +2,22 @@
  * Created by Gianmarco on 17/03/2016.
  */
 
-var http = require('http');
-var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+var express = require('express');
+var app = express();
+var fs = require("fs");
+
+app.get('/listUsers', function (req, res) {
+    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+        console.log( data );
+        res.end( data );
+    });
+})
+
+var server = app.listen(8081, function () {
+
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log("Example app listening at http://%s:%s", host, port)
+
+})
