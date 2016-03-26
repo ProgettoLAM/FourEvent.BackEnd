5acfd2d9effa484bc49936f0791ce039befcd0d0
Spino9330 <spino9330@gmail.com>
2016-03-26 14:39:16 +0100

    Inizializzazione codice on Azure

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-/**
- * Created by Gianmarco on 17/03/2016.
- */
+// Prova nodejs on Azure
+// Author: spino9330
+// Project: FourEvent.BackEnd
 
 var http = require('http')
 var port = process.env.PORT || 1337;
 http.createServer(function(req, res) {
 
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end("Hello World\nI'm in master");
+    res.end("Hello Matey!");
 
 }).listen(port);
