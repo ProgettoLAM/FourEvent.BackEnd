d8458c3fedf791d24edae9ee2d289e8f1f38fa74
Gianmarco Spinaci <spino@spino9330-mac.lan>
2016-05-04 19:27:06 +0200

    Added package Hapi

--- server.js
+++ server.js
@@ -1,26 +1,69 @@
-// Prova nodejs on Azure
-// Author: spino9330
-// Project: FourEvent.BackEnd
+var Hapi = require('hapi');
+var server = new Hapi.Server();
 
-//posto un commento così
-var fs = require("fs")
-var http = require('http')
+var quotes = [
+  {
+    author: 'Audrey Hepburn'
+  , text: 'Nothing is impossible, the word itself says \'I\'m possible\'!'
+  }
+, {
+    author: 'Walt Disney'
+  , text: 'You may not realize it when it happens, but a kick in the teeth may be the best thing in the world for you'
+  }
+, {
+    author: 'Unknown'
+  , text: 'Even the greatest was once a beginner. Don\'t be afraid to take that first step.'
+  }
+, {
+    author: 'Neale Donald Walsch'
+  , text: 'You are afraid to die, and you\'re afraid to live. What a way to exist.'
+  }
+];
 
-var port = process.env.PORT || 1337
 
-http.createServer(function(req, res) {
 
-    if(req.url === "/index"){
-       fs.readFile("index.html", function (err, data) {
-          res.writeHead(200, {'Content-Type': 'text/html'});
-          res.write(data);
-          res.end();
-       });
+server.connection({
+    host: 'localhost',
+    port: Number(process.argv[2] || 8080)
+});
+
+server.route({
+    method: 'GET',
+    path:'/',
+    handler: function (request, reply) {
+
+        return reply('Hello HAPI');
     }
-    else {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write('<b>Hey there!</b><br /><br />This is the default response. Requested URL is: ' + req.url);
-      res.end();
-    }
+});
 
-}).listen(port);
+server.route({
+    method: 'GET',
+    path: '/quote',
+    handler: function (request, reply) {
+        reply(quotes);
+    }
+});
+
+server.route({
+  method: 'GET'
+, path: '/quote/{id}'
+, handler: function(req, reply) {
+
+    if (quotes.length <= req.params.id) {
+        return reply('No quote found.').code(404);
+      }
+      return reply(quotes[req.params.id]);
+  }
+});
+
+server.route({
+    method: 'GET',
+    path: '/name/{name}',
+    handler: function (request, reply) {
+        reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
+    }
+});
+
+server.start(function () {
+    console.log('Server running at:', server.info.uri);
+});
